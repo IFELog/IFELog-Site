89352d438e1001f78c77773a6573ab9e8d935982
Wenyan <wenyan@teambition.com>
2016-03-09 20:07:40 +0800

    nav-list & search-box

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,48 @@
 <template>
   <div id="app">
-    <img class="logo" src="./assets/logo.png">
-    <hello></hello>
-    <p>
-      Welcome to your Vue.js app. To get started, take a look at the
-      <a href="https://github.com/vuejs-templates/webpack#folder-structure" target="_blank">README</a>
-      of this template. If you have any issues with the setup, please file an issue at this template's repository.
-    </p>
-    <p>
-      For advanced configurations, checkout the docs for
-      <a href="http://webpack.github.io/" target="_blank">Webpack</a> and
-      <a href="http://vuejs.github.io/vue-loader/" target="_blank">vue-loader</a>.
-    </p>
-    <p>
-      You may also want to checkout
-      <a href="https://github.com/vuejs/vue-router/" target="_blank">vue-router</a> for routing and
-      <a href="https://github.com/vuejs/vuex/" target="_blank">vuex</a> for state management.
-    </p>
+    <navlist></navlist>
+    <div class="container">
+      <search></search>
+    </div>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+import Navlist from './components/Nav'
+import Search from './components/Search'
 
 export default {
   components: {
-    Hello
+    Navlist, Search
   }
 }
 </script>
 
 <style lang="stylus">
-html
-  height: 100%
-  transform: scale(1)
+  *
+    margin: 0
+    padding: 0
 
-body {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
-}
+  ul, li
+    list-style: none
 
-#app {
-  margin-top: -100px;
-  max-width: 600px;
-  font-family: Helvetica, sans-serif;
-  text-align: center;
-}
+  html
+    height: 100%
+    transform: scale(1)
 
-.logo {
-  width: 100px;
-  height: 100px
-}
+  body
+    height: 100%
+    padding-top: 38px
+    overflow: hidden
+
+  #app
+    font-family: Helvetica, sans-serif
+
+
+  .container
+    margin-top: 200px
+    /*display: flex
+    align-items: center
+    justify-content: center*/
+
 </style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,46 @@
+<template>
+  <header>
+    <ul class="nav-list">
+      <li v-for="item in items">
+        <a href='{{ item.url }}'>{{ item.nav }}</a>
+      </li>
+    </ul>
+  </header>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      items: [
+        { nav: 'Home', url: 'aaa' },
+        { nav: 'About', url: 'aaa' },
+        { nav: 'GitHub', url: 'aaa' },
+        { nav: 'Content', url: 'aaa' }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+  header
+    width: 100%
+    position: fixed
+    top: 0
+    right: 0
+    font-size: 1.0em
+    padding: 0px 20px
+    ul
+      float: right
+    li
+      float: left
+      margin-left: 0.2em
+      padding: 10px 12px
+      a
+        color: #383838
+        &:hover
+          color: #000000
+
+
+</style>
